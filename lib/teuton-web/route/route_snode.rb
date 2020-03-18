1404e498b92fdda12abcc0401a6fc7ebe6cf3c64
dvarrui <dvarrui@protonmail.com>
2020-03-18 19:05:42 +0000

    U

--- lib/teuton-web/route/route_snode.rb
+++ lib/teuton-web/route/route_snode.rb
@@ -1,12 +1,37 @@
 
+require 'yaml'
+
 module Sinatra
   module Service
     module RouteSnode
       def self.registered(app)
         app.get '/snode' do
           @mode = :Snode
-          @current = File.join(Dir.pwd)
-          load_dir @current
+          @current = Dir.pwd
+          @list = []
+          filenames = Dir.glob(File.join(@current, "**")).sort!
+          filenames.each do |filepath|
+            item = { filename: File.basename(filepath) }
+            stat = File.stat(filepath).ctime
+            ctime = { year: stat.year, month: stat.month, day: stat.day,
+                      hour: stat.hour, min: stat.min, sec: stat.sec }
+            timestamp = format("%<year>04d-%<month>02d-%<day>02d %<hour>02d:%<min>02d:%<sec>02d", ctime)
+            item[:timestamp] = timestamp
+            item[:link] = true
+            item[:link] = false unless item[:filename].start_with? 'case-'
+            item[:ext] = File.extname(item[:filename])
+            item[:grade] = ''
+            item[:show] = false
+            if item[:ext] == '.yaml' && item[:link]
+              item[:show] = true
+              data = YAML.load_file(filepath)
+              item[:grade] = data[:results][:grade]
+            elsif item[:ext] == '.txt' && item[:link]
+              data = File.read(filepath).split("\n")
+              item[:grade] = data[data.size-2].split(' ')[3]
+            end
+            @list << item
+          end
           erb :"snode/index"
         end
 
@@ -14,6 +39,12 @@ module Sinatra
           @mode = :Snode
           content = File.read(params[:filename])
           "<pre>#{content}</pre>"
+        end
+
+        app.get '/snode/report/show/:filename' do
+          @mode = :Snode
+          @data = YAML.load_file(params[:filename])
+          erb :"snode/show"
         end
       end
     end
